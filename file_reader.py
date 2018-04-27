2d103001b248dba2b0bc22fdf61e94cc0d7d4269
Thanmai Bindi <tbindi@umail.iu.edu>
2018-04-27 00:24:02 -0700

    Modified File Reader to read State, Latitude and Longitude

--- file_reader.py
+++ file_reader.py
@@ -1,14 +1,30 @@
 import csv
 
+
 class Node(object):
-    def __init__(self, city, state):
+    def __init__(self, city, state, latitude=0, longitude=0):
         self.city = city
         self.state = state
+        self.latitude = latitude
+        self.longitude = longitude
 
-def List():
-     with open('citygps.csv','r') as file:
-        reader = csv.reader(file)
-        return [Node(city,state) for city, state in reader]
 
-for get in List():
-     print(get.city, get.state) 
+def read_file(file_name='citygps.csv'):
+	result = {}
+	with open(file_name, 'r') as file:
+		reader = csv.reader(file)
+		for city, details in reader:
+			state, latitude, longitude = details.split(' ')
+			result[city] = Node(city, state, latitude, longitude)
+	return result
+
+
+if __name__ == "__main__":
+	# Reads as City = Node(city, state, latitude, longitude)
+	input_data = read_file()
+	input_city = 'Seattle'
+	print "This Prints City : Node"
+	print input_data[input_city]
+	print input_data[input_city].state
+	print input_data[input_city].latitude
+	print input_data[input_city].longitude
